176ea56094f6b6d6950ceb7d3931432ac2bdb926
askrealcookie <47023698+xKukiMonster@users.noreply.github.com>
2021-04-10 01:37:23 +0200

    Update ArrayUtil.java

--- src/main/java/me/cookiemonster/zsocraft/zsocraftpartyhomes/util/ArrayUtil.java
+++ src/main/java/me/cookiemonster/zsocraft/zsocraftpartyhomes/util/ArrayUtil.java
@@ -1,10 +1,21 @@
 package me.cookiemonster.zsocraft.zsocraftpartyhomes.util;
 
+import java.util.List;
+import java.util.ListIterator;
+
 public class ArrayUtil {
     public static boolean contains(Object[] objects, Object object){
         for (int i = 0; i < objects.length; i++){
             if(objects[i] == object) return true;
         }
         return false;
+    }
+    public static void replaceToLowerCase(List<String> strings)
+    {
+        ListIterator<String> iterator = strings.listIterator();
+        while (iterator.hasNext())
+        {
+            iterator.set(iterator.next().toLowerCase());
+        }
     }
 }
