4df3f75ce27d1e89104da98613adcb69a9228c78
askrealcookie <47023698+xKukiMonster@users.noreply.github.com>
2021-04-09 21:54:47 +0200

    Already teleporting check

--- src/main/java/me/cookiemonster/zsocraft/zsocraftpartyhomes/ZSOCraftPartyHomes.java
+++ src/main/java/me/cookiemonster/zsocraft/zsocraftpartyhomes/ZSOCraftPartyHomes.java
@@ -2,7 +2,6 @@ package me.cookiemonster.zsocraft.zsocraftpartyhomes;
 
 import me.cookiemonster.zsocraft.zsocraftpartyhomes.command.PartyHomeCommand;
 import me.cookiemonster.zsocraft.zsocraftpartyhomes.command.PartySetHomeCommand;
-import me.cookiemonster.zsocraft.zsocraftpartyhomes.listener.PlayerChatListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ZSOCraftPartyHomes extends JavaPlugin {
@@ -14,7 +13,6 @@ public final class ZSOCraftPartyHomes extends JavaPlugin {
         instance = this;
 
         RegisterCommands();
-        RegisterEvents();
 
         saveDefaultConfig();
     }
@@ -26,10 +24,6 @@ public final class ZSOCraftPartyHomes extends JavaPlugin {
     private void RegisterCommands() {
         getCommand("partyhome").setExecutor(new PartyHomeCommand());
         getCommand("partysethome").setExecutor(new PartySetHomeCommand());
-    }
-
-    private void RegisterEvents(){
-        getServer().getPluginManager().registerEvents(new PlayerChatListener(), this);
     }
 
 
