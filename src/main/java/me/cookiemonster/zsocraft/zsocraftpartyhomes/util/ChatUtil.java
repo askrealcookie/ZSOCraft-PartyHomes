bd876de1f43ecd9b493848a1c9ff2862f0b7cbea
askrealcookie <47023698+xKukiMonster@users.noreply.github.com>
2021-04-10 01:37:25 +0200

    Update ChatUtil.java

--- src/main/java/me/cookiemonster/zsocraft/zsocraftpartyhomes/util/ChatUtil.java
+++ src/main/java/me/cookiemonster/zsocraft/zsocraftpartyhomes/util/ChatUtil.java
@@ -14,4 +14,12 @@ public final class ChatUtil {
     public static String locToCoords(Location loc){
         return "x: " + loc.getBlockX() + ", y: " + loc.getBlockY() + ", z: " + loc.getBlockZ();
     }
+
+    public static String ArrayToString(Object[] array){
+        StringBuilder s = new StringBuilder();
+        for(Object obj : array){
+            s.append(obj).append(" ");
+        }
+        return s.toString();
+    }
 }
