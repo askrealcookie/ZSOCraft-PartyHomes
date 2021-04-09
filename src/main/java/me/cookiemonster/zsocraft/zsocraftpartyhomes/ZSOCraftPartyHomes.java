8b2dac3a323f80b70bb7b6ec12b9deb6c48a471d
askrealcookie <47023698+xKukiMonster@users.noreply.github.com>
2021-04-09 20:13:06 +0200

    Update ZSOCraftPartyHomes.java

--- src/main/java/me/cookiemonster/zsocraft/zsocraftpartyhomes/ZSOCraftPartyHomes.java
+++ src/main/java/me/cookiemonster/zsocraft/zsocraftpartyhomes/ZSOCraftPartyHomes.java
@@ -2,22 +2,34 @@ package me.cookiemonster.zsocraft.zsocraftpartyhomes;
 
 import me.cookiemonster.zsocraft.zsocraftpartyhomes.command.PartyHomeCommand;
 import me.cookiemonster.zsocraft.zsocraftpartyhomes.command.PartySetHomeCommand;
+import me.cookiemonster.zsocraft.zsocraftpartyhomes.listener.PlayerChatListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ZSOCraftPartyHomes extends JavaPlugin {
 
-    public static ZSOCraftPartyHomes instance;
+    private static ZSOCraftPartyHomes instance;
 
     @Override
     public void onEnable() {
         instance = this;
+
         RegisterCommands();
+        RegisterEvents();
+
         saveDefaultConfig();
+    }
+
+    public static ZSOCraftPartyHomes getInstance(){
+        return instance;
     }
 
     private void RegisterCommands() {
         getCommand("partyhome").setExecutor(new PartyHomeCommand());
         getCommand("partysethome").setExecutor(new PartySetHomeCommand());
+    }
+
+    private void RegisterEvents(){
+        getServer().getPluginManager().registerEvents(new PlayerChatListener(), this);
     }
 
 
